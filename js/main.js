9a6558fb344d4b86947f2a04ebf199e65f7d3cb9
Victor Patru <patruvictor.mihai@gmail.com>
2022-05-04 22:57:57 +0300

    Add HTML, CSS and Javascript files

--- js/main.js
+++ js/main.js
@@ -1,82 +1,77 @@
-// Adding an event listener on the button which will run our getFetch function
+// Listening for user click on our button to trigger the getFetch function
 document.querySelector('button').addEventListener('click', getFetch)
 
-
-// Fetching and manipulating the data from the Open Foods Facts API
 function getFetch() {
-    // Get the barcode out of the user input box
-    let barcode = document.querySelector('#barcode').value
+    // Getting user's barcode and placing it in the variable barcode
+    const barcode = document.getElementById('barcode').value
 
-    // Make sure that the barcode is UPC compliant (12 digit barcode)
-    if (barcode.length !== 12) {
-        alert('Please input a valid 12 digit barcode!')
-        return
-    }
-
-    // Fetch from the Open Foods Facts API using the user's inputted barcode
+    // Fetch from the Open Food Facts API based on the user's inputted barcode
     fetch(`https://world.openfoodfacts.org/api/v0/product/${barcode}.json`)
     .then(res => res.json())
     .then(data => {
 
-        // Product found
         if (data.status === 1) {
-            /* Use the class ProductInfo to manipulate and 
-            display the information we get from our user. */
-            const item = new ProductInfo(data.product)
-            item.showInfo()
-            item.listIngredients()
-        } 
-        
-        // Product not found
-        else if (data.status === 0) {
-            alert(`Product ${barcode} was not found. Try another valid barcode!`)
+            // pass data.product into the ProductInfo class
+            // data.product.ingredients.forEach(item => console.log(item.text))
+            const product = new ProductInfo(data.product)
+            product.displayItem()
+            product.displayTable()
+        } else {
+            alert('Please input a valid barcode!')
+            return
         }
     })
     .catch(err => console.log(`error ${err}`))
 }
 
-
+// Creating the product information class that we are going to call if the user's barcode is valid
+// It does two things: displays the items in the DOM and create the table with the ingredients and the "Seed Oil Checker" side-by-side
 class ProductInfo {
-    // Setting our constructor with the values we get from the API object
-    constructor(productData) {
-        this.name = productData.product_name
-        this.ingredients = productData.ingredients
-        this.image = productData.image_url
+    constructor(productDetails) {
+        this.name = productDetails.product_name
+        this.ingredients = productDetails.ingredients
+        this.image = productDetails.image_url
     }
 
-    // Adding the product name and image that is associated with that barcode
-    showInfo() {
-        document.getElementById('product-img').src = this.image
+    displayItem() {
+        // Display product image and name in the DOM
         document.getElementById('product-name').innerText = this.name
+        document.getElementById('product-img').src = this.image
     }
 
-    listIngredients() {
-        let tableRef = document.getElementById('ingredient-table')
+    displayTable() {
+        const seedOilList = ['almond oil', 'argan oil', 'borage oil', 'castor oil', 'cherry pit oil', 'corn oil', 'cottonseed oil', 'linseed oil', 'grape seed oil', 'hemp oil', 'rapeseed oil', 'canola oil', 'safflower oil', 'sesame oil', 'sunflower oil', 'palm oil']        
+        const tableRef = document.getElementById('ingredient-table')
 
-        // Loop through the ingredients object
-        // Add each ingredient on a new table row with a side-by-side checker on whether the ingredient is vegetarian
-        for (let key in this.ingredients) {
-            let newRow = tableRef.insertRow(-1) // Adding a new row at the bottom of our table ('ingredient-table')
+        if (tableRef.rows.length > 1) {
+            tableRef.innerHTML = ''
+        }
 
-            // Ingredients Column
-            let newIngredientCell = newRow.insertCell(0) // Adding a new ingredient cell to the first column of our table
-            let newIngredientText = document.createTextNode(this.ingredients[key].text) // Creating the text we are going to next in our ingredients cell column
-            newIngredientCell.appendChild(newIngredientText)
+        for (const key in this.ingredients) {
+            // Creating the table reference variable so we can start dynamically generate the table
 
-            // Vegetarian cell status?
-            let vegStatus = this.ingredients[key].vegetarian == null ? 'unknown' : this.ingredients[key].vegetarian // Cleaning the vegetarian output
-            let newVegetarianCell = newRow.insertCell(1) // Adding a new vegetarian cell to the second column of our table
-            let newVegetarianText = document.createTextNode(vegStatus) // Creating the text we are going to nest in our vegeratin cell column
-            newVegetarianCell.appendChild(newVegetarianText)
+            // Add product ingredients column
+                // Making sure new ingredients are appended at the bottom of our table (-1 is shorthand for the end of a table)
+                let newIngredientRow = tableRef.insertRow(-1)
 
-            if (vegStatus === 'no') {
-                newVegetarianCell.classList.add('non-veg-item')
-            } else if (vegStatus === 'unknown' || vegStatus === 'maybe') {
-                newVegetarianCell.classList.add('unknown-item')
-            }
+                // Adding new ingredients within that cell (0 means that it will be placed in the first column of our dynamically generated table)
+                let newIngredientCell = newIngredientRow.insertCell(0)
 
+                // Creating the text for our table
+                let newIngredientText = document.createTextNode(this.ingredients[key].text)
+
+                // Appending the dynamically generated text to our current table
+                newIngredientCell.appendChild(newIngredientText)
+
+                if (seedOilList.includes(this.ingredients[key].text)) {
+                    newIngredientCell.classList.add('contains-seed-oil')
+                } else if (this.ingredients[key].text === 'sugar' || this.ingredients[key].text.includes('soya')) {
+                    newIngredientCell.classList.add('contains-sugar')
+                }
+
+                console.log(tableRef.rows.length)
 
         }
-            
+
     }
 }
